2980061a169e1e3cfa2999f4919635b5ddbf9748
Michael Hayes <mkhayes@gmail.com>
2012-11-16 09:53:46 -0500

    Updated some comments

--- edu.oswego.mapse.MainActivity/src/edu/oswego/mapse/MainActivity.java
+++ edu.oswego.mapse.MainActivity/src/edu/oswego/mapse/MainActivity.java
@@ -19,208 +19,211 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private TextView currentSpeedText;
-	private View background;
-	private Handler handler;
+    private TextView currentSpeedText;
+    private View background;
+    private Handler handler;
 
-	final Context context = this;
+    final Context context = this;
 
-	private int currentSpeed;
-	private int currentSpeedLimit;
-	private int speedingThreshold;
+    private int currentSpeed;
+    private int currentSpeedLimit;
+    private int speedingThreshold;
 
-	private ColorChangeController colorScale;
-	private RoadAnimator roadAnimator;
-	private AlertDialog alertDialog;
-	private SLMExtender slm;
-	private Button signSubmitButton;
+    private ColorChangeController colorScale;
+    private RoadAnimator roadAnimator;
+    private AlertDialog alertDialog;
+    private SLMExtender slm;
+    private Button signSubmitButton;
 
-	private boolean speedSimRunning = false;
-	private boolean alerted = false;
+    private boolean speedSimRunning = false;
+    private boolean alerted = false;
 
-	private SoundPool soundPool;
-	private int soundID;
-	boolean soundLoaded = false;
+    private SoundPool soundPool;
+    private int soundID;
+    boolean soundLoaded = false;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// Instantiate sound stuff
-		this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
-		soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
-			public void onLoadComplete(SoundPool soundPool, int sampleId,
-					int status) {
-				soundLoaded = true;
-			}
-		});
-		soundID = soundPool.load(this, R.raw.sound1, 1);
+        // Instantiate sound stuff
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+                public void onLoadComplete(SoundPool soundPool, int sampleId,
+                    int status) {
+                soundLoaded = true;
+                }
+                });
+        soundID = soundPool.load(this, R.raw.sound1, 1);
 
-		// Instantiate other stuff
-		currentSpeedText = (TextView) findViewById(R.id.textView1);
-		currentSpeed = 0;
-		currentSpeedLimit = 35;
-		speedingThreshold = 10;
-		background = findViewById(R.id.mainLayout);
-		handler = new Handler();
-		colorScale = new ColorChangeController(speedingThreshold);
-		roadAnimator = new RoadAnimator(handler, this, findViewById(R.id.middleLine), currentSpeed);
+        // Instantiate other stuff
+        currentSpeedText = (TextView) findViewById(R.id.textView1);
+        currentSpeed = 0;
+        currentSpeedLimit = 55;
+        speedingThreshold = 10;
+        background = findViewById(R.id.mainLayout);
+        handler = new Handler();
+        colorScale = new ColorChangeController(speedingThreshold);
+        roadAnimator = new RoadAnimator(handler, this, findViewById(R.id.middleLine), currentSpeed);
 
-		// Build speeding alert dialog
-		alertDialog = new AlertDialog.Builder(this).setTitle("Caution")
-				.setMessage("You are speeding!").setCancelable(false).create();
+        // Build speeding alert dialog
+        alertDialog = new AlertDialog.Builder(this).setTitle("Caution")
+            .setMessage("You are speeding!").setCancelable(false).create();
 
-		// Instantiate a new manager for the wikispeedia database
-		try {
-			slm = new SLMExtender(getBaseContext());
+        // Instantiate a new manager for the wikispeedia database
+        try {
+            slm = new SLMExtender(getBaseContext());
 
-			// Create a listener to notify of speedlimit changes
-			SpeedlimitListener sll = new SpeedlimitListener() {
-				public void onSpeedLimitChanged(Integer speedlimit,
-						String copyright) {
-					// Everything below is called if there is a new sign
-					// returned
-				}
-			};
-			slm.requestChanges(sll);
-		} catch (Exception e) {
-			Log.e("Thing", e.getMessage());
-		}
-		
-		startSpeedChangeSimulation(this, 60, 1, 300);
+            // Create a listener to notify of speedlimit changes
+            SpeedlimitListener sll = new SpeedlimitListener() {
+                public void onSpeedLimitChanged(Integer speedlimit,
+                        String copyright) {
+                    // Everything below is called if there is a new sign
+                    // returned
+                }
+            };
+            slm.requestChanges(sll);
+        } catch (Exception e) {
+            Log.e("Thing", e.getMessage());
+        }
 
-		signSubmitButton = (Button) findViewById(R.id.signSubmitButton);
-		// Listening to button event
-		signSubmitButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View arg0) {
-				try {
-					Intent i = new Intent(MainActivity.this,
-							SignSubmitter.class);
-					i.putExtra("slm", slm);
-					startActivity(i);
-				} catch (Exception e) {
-				}
-			}// end onClick
-		});// end new onclick listener
-		
+        startSpeedChangeSimulation(this, 68, 1, 300);
 
-		startSpeedChangeSimulation(this, 30, -1, 500);
-		
-	}
+        signSubmitButton = (Button) findViewById(R.id.signSubmitButton);
+        // Listening to button event
+        signSubmitButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View arg0) {
+                try {
+                Intent i = new Intent(MainActivity.this,
+                    SignSubmitter.class);
+                i.putExtra("slm", slm);
+                startActivity(i);
+                } catch (Exception e) {
+                }
+                }// end onClick
+                });// end new onclick listener
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
 
-	@Override
-	public void onResume() {
-		super.onResume();
+        startSpeedChangeSimulation(this, 30, -1, 500);
 
-	}
+    }
 
-	/*
-	 * Simulates driving speed change by calling updateSpeed() repeatedly. Needs
-	 * reference to MainActivity. The delay between each speed change is in
-	 * milliseconds.
-	 */
-	private void startSpeedChangeSimulation(final MainActivity activity,
-			final int range, final int direction, final int rate) {
-		Runnable runnable = new Runnable() {
-			public void run() {
-				for (;;) {
-					if (activity.speedSimRunning == true) {
-						try {
-							Thread.sleep(10);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					} else {
-						activity.speedSimRunning = true;
-						break;
-					}
+@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
-				}
+@Override
+    public void onResume() {
+        super.onResume();
 
-				int speed = currentSpeed;
-				for (int i = 0; i < range; i++) {
-					speed += direction;
-					final int value = speed;
-					try {
-						Thread.sleep(rate);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					handler.post(new Runnable() {
-						public void run() {
-							activity.updateSpeed(value);
-						}
-					});
-				}
-				activity.speedSimRunning = false;
-			}
-		};
-		new Thread(runnable).start();
-	}
+    }
 
-	public void updateSpeed(int speed) {
-		currentSpeed = speed;
-		roadAnimator.updateSpeed(speed);
-		currentSpeedText.setText(Integer.toString(speed));
-		int color = colorScale.getColor(currentSpeedLimit, currentSpeed);
-		background.setBackgroundColor(color);
+    /*
+     * Simulates driving speed change by calling updateSpeed() repeatedly. Needs
+     * reference to MainActivity. The delay between each speed change is in
+     * milliseconds.
+     * Range is the amount the speed will change by.
+     * Direction is positive or negative (if you want to give -1, to speed up use 1).
+     * Rate is the time between each update in milliseconds and can be though of as speed.
+     */
+    private void startSpeedChangeSimulation(final MainActivity activity,
+            final int range, final int direction, final int rate) {
+        Runnable runnable = new Runnable() {
+            public void run() {
+                for (;;) {
+                    if (activity.speedSimRunning == true) {
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        activity.speedSimRunning = true;
+                        break;
+                    }
 
-		if (currentSpeed - currentSpeedLimit > speedingThreshold) {
-			if (!alerted) {
-				alerted = true;
-				alertDialog.show();
-				playAlertSound();
-			}
-		} else {
-			if (alerted) {
-				alerted = false;
-				alertDialog.cancel();
-			}
-		}
-	}
+                }
 
-	/*
-	 * Starts new thread to play alert sound in a loop. Thread terminates when
-	 * alerted is set to false by updateSpeed()
-	 */
-	private void playAlertSound() {
-		Runnable runnable = new Runnable() {
-			public void run() {
+                int speed = currentSpeed;
+                for (int i = 0; i < range; i++) {
+                    speed += direction;
+                    final int value = speed;
+                    try {
+                        Thread.sleep(rate);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.post(new Runnable() {
+                            public void run() {
+                            activity.updateSpeed(value);
+                            }
+                            });
+                }
+                activity.speedSimRunning = false;
+            }
+        };
+        new Thread(runnable).start();
+    }
 
-				AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-				float actualVolume = (float) audioManager
-						.getStreamVolume(AudioManager.STREAM_MUSIC);
-				float maxVolume = (float) audioManager
-						.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-				float volume = actualVolume / maxVolume;
-				// Is the sound loaded already?
+    public void updateSpeed(int speed) {
+        currentSpeed = speed;
+        roadAnimator.updateSpeed(speed);
+        currentSpeedText.setText(Integer.toString(speed));
+        int color = colorScale.getColor(currentSpeedLimit, currentSpeed);
+        background.setBackgroundColor(color);
 
-				for (;;) {
-					if (alerted) {
-						if (soundLoaded) {
-							soundPool.play(soundID, volume, volume, 1, 0, 1f);
-						}
+        if (currentSpeed - currentSpeedLimit > speedingThreshold) {
+            if (!alerted) {
+                alerted = true;
+                alertDialog.show();
+                playAlertSound();
+            }
+        } else {
+            if (alerted) {
+                alerted = false;
+                alertDialog.cancel();
+            }
+        }
+    }
 
-						try {
-							Thread.sleep(1500);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					} else {
-						break;
-					}
-				}
-			}
-		};
-		new Thread(runnable).start();
-	}
+    /*
+     * Starts new thread to play alert sound in a loop. Thread terminates when
+     * alerted is set to false by updateSpeed()
+     */
+    private void playAlertSound() {
+        Runnable runnable = new Runnable() {
+            public void run() {
+
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                // Is the sound loaded already?
+
+                for (;;) {
+                    if (alerted) {
+                        if (soundLoaded) {
+                            soundPool.play(soundID, volume, volume, 1, 0, 1f);
+                        }
+
+                        try {
+                            Thread.sleep(1500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
 
 }

20cc34109005821398d8b9fa9c8df5dec271c2d6
Adam <adamwilhelmi@gmail.com>
2012-11-18 21:28:02 -0500

    hmm....

--- MAPSE/project_src/MAPSE/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/AndroidManifest.xml : \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/AndroidManifest.xml \

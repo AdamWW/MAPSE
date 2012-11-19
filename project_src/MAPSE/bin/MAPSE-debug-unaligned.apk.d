dd8e12ba72df433057e3942d2b6e07f36ae17e8d
Adam <adamwilhelmi@gmail.com>
2012-11-19 12:30:21 -0500

    deletes...

--- project_src/MAPSE/bin/MAPSE-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/MAPSE-debug-unaligned.apk : \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/MAPSE.ap_ \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes.dex \
-/home/adam/Android/android-sdk-linux/tools/support/annotations.jar \
--- project_src/MAPSE/bin/classes.dex.d
+++
@@ -1,8 +0,0 @@
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes.dex : \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes/mustard/mapse/BuildConfig.class \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes/mustard/mapse/R$attr.class \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes/mustard/mapse/R.class \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes/mustard/mapse/R$layout.class \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes/mustard/mapse/main.class \
-/home/adam/Dropbox/Oswego/Fall2012/CSC380/MAPSE/project_src/MAPSE/bin/classes/mustard/mapse/R$string.class \
-/home/adam/Android/android-sdk-linux/tools/support/annotations.jar \
